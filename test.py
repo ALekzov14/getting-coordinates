e1987bf5146e228cca683dbf2fbeee943476a953
ALekzov <151780073+ALekzov14@users.noreply.github.com>
2024-10-07 18:43:05 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 from pynput import mouse
+import tkinter as tk
+from tkinter import scrolledtext
 
-def mouse_click(x, y, button, pressed):
+def add_text(x, y, button, pressed):
     if button == mouse.Button.left or mouse.Button.right and pressed:
-        print(f'X:{x} Y:{y}')
+        txt.insert(tk.END, f"X:{x} Y:{y}" + "\n")
 
-listener = mouse.Listener(on_click=mouse_click)
+listener = mouse.Listener(on_click=add_text)
+
+try:
+    from tkinter import*
+except:
+    from tkinter import*
+
+window = Tk()
+window.title("test")
+window.geometry("390x240")
+
+# Создание scrolledtext
+txt = scrolledtext.ScrolledText(window, width=40, height=14)
+txt.pack()
+
 listener.start()
-listener.join()
+window.attributes("-topmost",True)
+
+window.mainloop()
